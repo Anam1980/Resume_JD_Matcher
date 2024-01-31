d70185dc4784cebd518470932a9fbfe233a70957
Anam Sheikh <anamsheikh198@gmail.com>
2024-01-31 18:11:52 +0530

    Empty Project

---
+++ src/main/java/com/example/Resume_JD_Matcher/ResumeJdMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.Resume_JD_Matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeJdMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeJdMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Resume_JD_Matcher/ResumeJdMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Resume_JD_Matcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeJdMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
